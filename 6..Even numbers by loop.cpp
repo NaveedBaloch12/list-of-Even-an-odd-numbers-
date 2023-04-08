08938b342cc06c0f3a6b6101a400b17b13989307
Naveed Hassan <120362779+NaveedBaloch12@users.noreply.github.com>
2023-04-08 21:19:30 +0500

    Add files via upload

---
+++ 6..Even numbers by loop.cpp
@@ -0,0 +1,11 @@
+//Code for even Numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"This Program will give even Numbers"<<endl;
+	for(int i=0; i<=20; i=i+2)
+	{
+		cout<<i<<endl;
+	}
+}
---
+++ 6..Odd Numbers by loop.cpp
@@ -0,0 +1,12 @@
+//Odd numbers
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"This Program will give Odd numbers\n";
+	for(int i=1; i<20; i++)
+	{
+		cout<<(2*i)+1<<"\n";
+	}
+}
